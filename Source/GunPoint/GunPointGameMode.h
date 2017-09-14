86ae3c50455be1bd5d72311ad82056a80d6dd3bb
DESKTOP-EIUUUMV\korbe <korbenwids@gmail.com>
2017-09-13 21:13:47 -0400

    Initial Commit

---
+++ Source/GunPoint/GunPointGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GunPointGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGunPointGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGunPointGameMode();
+};
+
+
+
